bf3f54a19716922d4e94135faa46b2122720aa34
Aleksandry Cesar <57228025+KoiGunner@users.noreply.github.com>
2024-04-29 15:20:37 -0300

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+/*
+  Crie uma classe chamada Rectangle para representar um triangulo
+
+  O retângulo deve armazenar as informações do tamanho dos seus lados
+
+  Para criar um retângulo, os tamanhos dos lados devem ser fornecidos
+
+  Alternativamente, se ambos os lados forem do mesmo tamanho, apenas um valor pode ser forneceido
+  Depois de criado, os lados não podem ser alterados
+
+  Crie um método square() , que retorna a área do triângulo (basta multiplicar os tamanhos dos dois lados).
+
+  Crie um programa que exemplifica o que foi citado acima 
+*/
+
+public class Rectangle{
+  private final double width;
+  private final double height;
+
+  public Rectangle(double width, double height){
+    this.width = width;
+    this.height = height;
+  }
+
+  public Rectangle(double width){
+    this(width,width);
+  }
+
+  public double square(){
+    return width * height;
+  }  
+}
