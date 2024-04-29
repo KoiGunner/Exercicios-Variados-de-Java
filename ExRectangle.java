f543ce2d08649b152fb10cadcc762a23791cfae3
Aleksandry Cesar <57228025+KoiGunner@users.noreply.github.com>
2024-04-29 15:30:33 -0300

    Create ExRectangle.java

---
+++ ExRectangle.java
@@ -0,0 +1,23 @@
+/*
+  Crie uma classe chamada Rectangle para representar um triangulo
+
+  O retângulo deve armazenar as informações do tamanho dos seus lados
+
+  Para criar um retângulo, os tamanhos dos lados devem ser fornecidos
+
+  Alternativamente, se ambos os lados forem do mesmo tamanho, apenas um valor pode ser forneceido
+  Depois de criado, os lados não podem ser alterados
+
+  Crie um método square() , que retorna a área do triângulo (basta multiplicar os tamanhos dos dois lados).
+
+  Crie um programa que exemplifica o que foi citado acima 
+*/
+
+public class ExRectangle{
+  public static void main(String[] args){
+
+    var r1 = new Rectangle(10, 25);
+
+    var r2 = new Rectangle(2, 5);
+  }
+}
